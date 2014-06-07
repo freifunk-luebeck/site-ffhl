ba0cf9ced8abd11cd1f6c8723bee35ec5581f4c6
eriu <michel@vorsprach.de>
2014-06-07 18:40:17 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -16,7 +16,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.26
+DEFAULT_GLUON_RELEASE := 0.27
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
