0c10df5f607d80bf372a378cd8581b82d5e4e73b
Daniel Ehlers <ehlers@rz.uni-kiel.de>
2014-10-02 18:37:53 +0200

    Update for v0.6~exp

--- site.mk
+++ site.mk
@@ -1,9 +1,13 @@
 GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-14 \
 	gluon-alfred \
 	gluon-announced \
 	gluon-autoupdater \
-	gluon-luci-portconfig \
-	gluon-config-mode \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-legacy \
@@ -11,20 +15,17 @@ GLUON_SITE_PACKAGES := \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
 	gluon-next-node \
-	gluon-ath9k-workaround \
-	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
 	gluon-status-page \
 	iwinfo \
 	iptables \
-	haveged 
+	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.5.0+0-exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.6~exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
-
 
 GLUON_PRIORITY ?= 0
