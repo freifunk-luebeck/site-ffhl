2c77fd1345763acec5949396e0561e61589c843f
Nils Schneider <nils@nilsschneider.net>
2015-08-19 22:11:09 +0200

    Release v0.7.1

--- site.mk
+++ site.mk
@@ -26,7 +26,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.7
+DEFAULT_GLUON_RELEASE := 0.7.1
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
