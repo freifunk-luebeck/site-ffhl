adce836e5baa9fd144445c1a5ef147e2bad909cb
FreifunkUFO <ufo@rund.freifunk.net>
2016-01-17 23:03:53 +0100

    neue release nummer für neues release

--- site.mk
+++ site.mk
@@ -33,7 +33,7 @@ DEFAULT_GLUON_CHECKOUT := master
 # Allow overriding the checkout from the command line
 GLUON_CHECKOUT ?= $(DEFAULT_GLUON_CHECKOUT)
 
-DEFAULT_GLUON_RELEASE := 0.8+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.9+exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
