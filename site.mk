d3002c585109e682eb6ebf6e576a95cf96c352b4
Linus Lüssing <linus.luessing@c0d3.blue>
2017-01-31 04:55:53 +0100

    Add package gluon-ebtables-segment-mld

--- site.mk
+++ site.mk
@@ -10,6 +10,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-ebtables-segment-mld \
 	gluon-legacy \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
