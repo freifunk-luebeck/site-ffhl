3d7b2caf86e879f37fdd77e8c185546085b75d13
Matthias Schiffer <mschiffer@universe-factory.net>
2017-04-20 21:26:47 +0200

    Release v0.9.3

--- site.mk
+++ site.mk
@@ -28,7 +28,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.9.2
+DEFAULT_GLUON_RELEASE := 0.9.3
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
