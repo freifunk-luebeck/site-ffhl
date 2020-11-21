11b2ae02de67a2f16d8a0ff43fe133a17253b6da
Paul Maruhn <paulmaruhn@posteo.de>
2020-11-21 20:06:39 +0100

    update ci

---
+++ build-images.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+TARGETS=$(make list-targets)
+
+FLAGS="-j $(nproc)"
+#FLAGS="-j 1 V=sc"
+
+make update
+
+for target in $TARGETS; do
+	echo "====================="
+	echo "building $target"
+	echo "====================="
+
+	make $FLAGS GLUON_TARGET=$target
+done
+
+
+make manifest GLUON_BRANCH=experimental GLUON_PRIORITY=0
+make manifest GLUON_BRANCH=beta GLUON_PRIORITY=0
+make manifest GLUON_BRANCH=stable GLUON_PRIORITY=7
