cfcc426a3d981a9a7df814a57e617918b7379d42
Paul Maruhn <paulmaruhn@posteo.de>
2021-06-21 00:22:52 +0200

    update ci

--- scripts/generate-ci.py
+++ scripts/generate-ci.py
@@ -16,6 +16,8 @@ BEFORE_SCRIPT = [
 	"apt-get install -y curl git tree > /dev/null",
 ]
 
+VARIABLES = {}
+
 ci = {
 	"image": "cuechan/gluon-build:latest",
 	"default": {
@@ -23,50 +25,40 @@ ci = {
 	},
 	"variables": {
 		"GIT_SUBMODULE_STRATEGY": "recursive",
+		"FORCE_UNSAFE_CONFIGURE": "1",
 	},
 	"stages": [
 		"build",
 		"test",
 		"deploy",
 		"upload",
+	]
+}
+
+ci['build-all'] = {
+	"stage": "build",
+	"tags": ["fast"],
+	"variables": {
+		"GLUON_SITEDIR": "..",
+		"GLUON_DEPRECATED": 1,
+		"GLUON_AUTOUPDATER_ENABLED": 1,
+		"GLUON_AUTOUPDATER_BRANCH": "stable",
+		"GLUON_BRANCH": "$GLUON_AUTOUPDATER_BRANCH",
+	},
+	"before_script": BEFORE_SCRIPT,
+	"parallel": {
+		"matrix": [
+			{"TARGET": get_available_targets()}
+		]
+	},
+	"script": [
+		"tree -L 3",
+		"make -C gluon update",
+		"make -C gluon -j $((($(nproc)+1) / 2)) GLUON_TARGET=$TARGET ",
 	],
-	"build-all": {
-		"stage": "build",
-		"tags": ["fast"],
-		# "variables": {
-		# 	""
-		# },
-		"before_script": BEFORE_SCRIPT,
-		"parallel": {
-			"matrix": [
-				{"TARGET": get_available_targets()}
-			]
-		},
-		"script": [
-			"tree -L 3",
-			"GLUON_TARGET=$TARGET ./scripts/build-images.sh",
-		],
-		# "script": [
-		# 	"tree -L 3",
-		# 	"mkdir -p gluon/output/{debug,images,packages}",
-		# 	"mkdir -p gluon/output/images/{factory,other,sysupgrade}",
-		# 	"for i in {0..20}; do touch gluon/output/images/factory/gluon_${i}.bin; done",
-		# 	"for i in {0..30}; do touch gluon/output/images/sysupgrade/gluon_${i}.bin; done",
-		# 	"for i in {0..10}; do touch gluon/output/images/other/gluon_${i}.bin; done",
-		# 	"mkdir -p gluon/output/packages/$TARGET/$TARGET",
-		# 	"for i in {0..10}; do touch gluon/output/packages/$TARGET/$TARGET/package_${i}.bin; done",
-		# ],
-		"cache": {
-			"paths": [
-				# "gluon/openwrt",
-				# "gluon/tmp",
-				# "gluon/packages"
-			],
-		},
-		"artifacts": {
-			"when": "always",
-			"paths": ["gluon/output"]
-		}
+	"artifacts": {
+		"when": "always",
+		"paths": ["gluon/output"]
 	}
 }
 
