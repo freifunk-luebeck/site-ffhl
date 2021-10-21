8356ee55150f89558ede66519774a63eee663cf3
Paul Maruhn <paulmaruhn@posteo.de>
2021-10-21 22:13:13 +0200

    fix build variables

--- site.mk
+++ site.mk
@@ -25,9 +25,10 @@ DEFAULT_GLUON_RELEASE := 0.14.2~exp$(shell date '+%Y%m%d')
 
 # gitlab-ci: use commit tag, if avalable as version number
 GLUON_RELEASE ?= $(CI_COMMIT_TAG)
+ifeq $($(GLUON_RELEASE),)
+	GLUON_RELEASE := $(DEFAULT_GLUON_RELEASE)
+endif
 
-# Allow overriding the release number from the command line
-GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
 GLUON_AUTOUPDATER_ENABLED = 1
 GLUON_AUTOUPDATER_BRANCH ?= stable
